a08f9944d6809ce9435bb5612d229b38ecd4c69d
Jason <theniteowl@gmail.com>
2023-07-09 20:22:44 -0700

    animation left and right, jumping, move left and right, gravity

--- Player.cs
+++ Player.cs
@@ -1,19 +1,65 @@
 using Godot;
 using System;
 
-public partial class Player : Area2D
+public partial class Player : CharacterBody2D
 {
 	private AnimatedSprite2D _animatedSprite;
-	
+	int SCREEN_HORIZ = 500;
+	int SCREEN_VERT = 500;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_animatedSprite = GetNode<AnimatedSprite2D>("player_animation");
 	}
 
+	public override void _PhysicsProcess(double delta)
+	{
+		// Move down 1 pixel per physics frame
+		MoveAndCollide(new Vector2(0, 1));
+	}
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		_animatedSprite.Play("standing_still");
+		int HORIZ_MOVEMENT = 100;
+		int VERT_MOVEMENT = 1000;
+
+		var velocity = Vector2.Zero; // The player's movement vector.
+
+		if (Input.IsActionPressed("jump"))
+		{
+			velocity.Y -= VERT_MOVEMENT;
+		}
+
+		if (Input.IsActionPressed("right"))
+		{
+			velocity.X += HORIZ_MOVEMENT;
+			_animatedSprite.FlipH = false;
+			_animatedSprite.Play("walking");
+
+		}
+		else if (Input.IsActionPressed("left"))
+		{
+			velocity.X -= HORIZ_MOVEMENT;
+			_animatedSprite.FlipH = true;
+			_animatedSprite.Play("walking");
+		}
+		else
+		{
+			_animatedSprite.Stop();
+		}
+
+		ApplyGravity(velocity, delta);
+	}
+
+	private void ApplyGravity(Vector2 velocity, double delta)
+	{
+		// translate new vector to this player's movement
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, SCREEN_HORIZ),
+			y: Mathf.Clamp(Position.Y, 0, SCREEN_VERT)
+		);
 	}
 }
