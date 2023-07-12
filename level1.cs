56baf235f1c60a98a74e71eb15d134f3901aa37e
Jason <theniteowl@gmail.com>
2023-07-12 00:41:36 -0700

    -setting the project display height too large makes shooting not work? reverting for now - basic proc gen to layout platforms, more lower down - TODO: figure out climbability of the level

--- level1.cs
+++ level1.cs
@@ -4,16 +4,16 @@ using System;
 public partial class level1 : Node2D
 {
 	static int BLOCK_WIDTH = 54;
-	static int BLOCK_HEIGHT = 7;
+	static int BLOCK_HEIGHT = 10;
 	private PackedScene _blockScene = (PackedScene)GD.Load("res://block.tscn");
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Player player = (Player)GetNode("Player");
 		Vector2 PlayerPos = Vector2.Zero;
-		PlayerPos.Y = (int)GetViewportRect().Size.Y -100;
+		PlayerPos.Y = (int)GetViewportRect().Size.Y - 100;
 		player.Position = PlayerPos;
-		
+
 		MakeFloor();
 
 		ScatterBlocks();
@@ -21,25 +21,40 @@ public partial class level1 : Node2D
 
 	private void ScatterBlocks()
 	{
-		for (int i = 0; i < 20; i++)
+		int TotalRows = (int)(int)GetViewportRect().Size.Y / BLOCK_HEIGHT;
+		int TotalCols = (int)(int)GetViewportRect().Size.X / BLOCK_WIDTH;
+
+		GD.Print(TotalCols);
+		GD.Print(TotalRows);
+
+		for (int r = 0; r < TotalRows; r++)
 		{
-			// create a set of rows with a player's height between them
-			// randomly 
-			Random rnd = new Random();
-			int RanX = rnd.Next(0, (int)GetViewportRect().Size.X);
-			int RanY = rnd.Next(0, (int)GetViewportRect().Size.Y - 64);
-			Node2D block = (Node2D)_blockScene.Instantiate();
-			block.Position = new Vector2(x: RanX, y: RanY);
-			AddChild(block);
+			if (r % 10 == 0)
+			{
+				for (int c = 0; c < TotalCols; c++)
+				{
+					Random rnd = new Random();
+					// more likely to have a block lower down
+					int BaselineRowPercent = (int)(((float)r / (float)TotalRows) * 100);
+					int PercentShouldMake = rnd.Next(BaselineRowPercent, 100);
+
+					if (PercentShouldMake > 80)
+					{
+						int FiftyFifty = rnd.Next(0, 10);
+						GD.Print(FiftyFifty);
+						
+						if (FiftyFifty >= 5)
+						{
+							Node2D block = (Node2D)_blockScene.Instantiate();
+							block.Position = new Vector2(x: c * BLOCK_WIDTH, y: r * BLOCK_HEIGHT);
+							GD.Print("Making a block at : " + block.Position);
+							AddChild(block);
+						}
+					}
+				}
+			}
 		}
 
-
-		for (int i = 0; i < 30; i++)
-		{
-			Node2D block = (Node2D)_blockScene.Instantiate();
-			block.Position = new Vector2(x: 0, y: i * (BLOCK_HEIGHT + 30));
-			AddChild(block);
-		}
 	}
 
 	private void MakeFloor()
@@ -54,7 +69,7 @@ public partial class level1 : Node2D
 			Node2D block = (Node2D)_blockScene.Instantiate();
 			block.Position = new Vector2(x: FloorLength, y: (int)GetViewportRect().Size.Y);
 			AddChild(block);
-			
+
 			i++;
 		}
 	}
