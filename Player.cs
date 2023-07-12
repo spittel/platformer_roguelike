c06912e7212262b4942025e1203265a3688b7d07
Jason <theniteowl@gmail.com>
2023-07-11 19:13:30 -0700

    - can hold jump to jump higher - can no longer jump infinite - jump enabled when colliding with anything (wall jumping will work)

--- Player.cs
+++ Player.cs
@@ -4,17 +4,19 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	private AnimatedSprite2D _animatedSprite;
-	int SCREEN_HORIZ = 1000;
-	int SCREEN_VERT = 500;
-	private const float GRAVITY = 1000.0f;
+	private const float GRAVITY = 9000.0f;
 	int HORIZ_MOVEMENT = 100;
-	int VERT_MOVEMENT = 1000;
+	int JUMP_MOVEMENT = 1000;
 	bool _isRight = true;
 
 	static double FIRE_WAIT = .1;
 	double _lastFire = FIRE_WAIT + .0001;
+	static int JUMP_NUM = 10;
+	int _currentJump = 0;
 
 	private PackedScene _bulletScene = (PackedScene)GD.Load("res://bullet.tscn");
+	private KinematicCollision2D _collisionInfo;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -22,22 +24,20 @@ public partial class Player : CharacterBody2D
 	}
 
 	public override void _PhysicsProcess(double delta)
-	{		
+	{
 		Fire(delta);
-		// Move down 1 pixel per physics frame
-		MoveAndCollide(new Vector2(0, 1));
+		_collisionInfo = MoveAndCollide(new Vector2(0, 0));
+
+		if (_collisionInfo == null)
+			GD.Print(_collisionInfo);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		// GD.Print("processing on player side");
 		var velocity = Vector2.Zero; // The player's movement vector.
 
-		if (Input.IsActionPressed("jump"))
-		{
-			velocity.Y -= VERT_MOVEMENT;
-		}
+		velocity = Jump(delta, velocity);
 
 		if (Input.IsActionPressed("right"))
 		{
@@ -62,12 +62,35 @@ public partial class Player : CharacterBody2D
 		ApplyMovement(velocity, delta);
 	}
 
+	private Vector2 Jump(double delta, Vector2 velocity)
+	{
+		if (Input.IsActionPressed("jump"))
+		{
+			if ( _currentJump < JUMP_NUM
+				|| _collisionInfo != null // on ground, or colliding with something
+				)
+			{
+				velocity.Y -= JUMP_MOVEMENT;
+				
+				_currentJump++;
+
+				// player can hold jump down and jump a little higher, to the equivalent of 
+				// JUMP_NUM
+				if (_currentJump > JUMP_NUM)
+					_currentJump = 0;
+			}
+		}
+
+		return velocity;
+	}
+
 	private void Fire(double delta)
 	{
 		_lastFire += delta;
 
 		// limit firing rate
-		if(_lastFire < FIRE_WAIT){
+		if (_lastFire < FIRE_WAIT)
+		{
 			return;
 		}
 
@@ -82,7 +105,7 @@ public partial class Player : CharacterBody2D
 			{
 				bullet.SetDirection(bullet.LEFT);
 			}
-			bullet.Transform = GlobalTransform;  
+			bullet.Transform = GlobalTransform;
 			Owner.AddChild(bullet);
 			_lastFire = 0;
 		}
@@ -94,8 +117,8 @@ public partial class Player : CharacterBody2D
 		// translate new vector to this player's movement
 		Position += velocity * (float)delta;
 		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, 0, SCREEN_HORIZ),
-			y: Mathf.Clamp(Position.Y, 0, SCREEN_VERT)
+			x: Mathf.Clamp(Position.X, 0, (int)GetViewportRect().Size.X),
+			y: Mathf.Clamp(Position.Y, 0, (int)GetViewportRect().Size.Y)
 		);
 	}
 }
--- level1.cs
+++ level1.cs
@@ -7,16 +7,32 @@ public partial class level1 : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		for (int i = 0; i < 10; i++)
+		MakeFloor();
+
+		ScatterBlocks();
+	}
+
+	private void ScatterBlocks()
+	{
+		for (int i = 0; i < 20; i++)
 		{
 			Random rnd = new Random();
-			int RanX = rnd.Next(0, 1000);
-			int RanY = rnd.Next(0, 800);
+			int RanX = rnd.Next(0, (int)GetViewportRect().Size.X);
+			int RanY = rnd.Next(0, (int)GetViewportRect().Size.Y - 64);
 			Node2D block = (Node2D)_blockScene.Instantiate();
 			block.Position = new Vector2(x: RanX, y: RanY);
 			AddChild(block);
 		}
+	}
 
+	private void MakeFloor()
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			Node2D block = (Node2D)_blockScene.Instantiate();
+			block.Position = new Vector2(x: i * 64, y: (int)GetViewportRect().Size.Y);
+			AddChild(block);
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
