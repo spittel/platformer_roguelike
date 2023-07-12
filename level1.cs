bc47e7de3c4679109460714ccafe916e3aa3f808
Jason <theniteowl@gmail.com>
2023-07-11 21:24:11 -0700

    - added new rad hover platform - adjusted hitboxes

--- level1.cs
+++ level1.cs
@@ -7,6 +7,7 @@ public partial class level1 : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Viewport.wor
 		MakeFloor();
 
 		ScatterBlocks();
