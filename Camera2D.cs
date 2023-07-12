a7a5fb92ee7967190462fe67b328c9e67f0335c5
Jason <theniteowl@gmail.com>
2023-07-11 21:43:11 -0700

    camera follows player 1 on the y axis

--- Camera2D.cs
+++ Camera2D.cs
@@ -3,16 +3,23 @@ using System;
 
 public partial class Camera2D : Godot.Camera2D
 {
+	int MID_WAY = 0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		MID_WAY = (int)GetViewportRect().Size.X / 2;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Player p  = (Player)GetNode("../Player");
+		Vector2 CameraPosition = Vector2.Zero;
 
-		Position = p.Position;
+		Player p = (Player)GetNode("../Player");
+
+		CameraPosition.Y = p.Position.Y;
+		CameraPosition.X = MID_WAY;
+
+		Position = CameraPosition;
 	}
 }
