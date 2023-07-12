234e579cc0c0981a3acbfa3a0f71d13bf67aa872
Jason <theniteowl@gmail.com>
2023-07-11 23:32:52 -0700

    - made floor go across entire view port - made total level height much larger than viewport (stretch aspect to keep_height) - spawn player near bottom so he doesnt drop for 10 s

--- level1.cs
+++ level1.cs
@@ -4,10 +4,16 @@ using System;
 public partial class level1 : Node2D
 {
 	static int BLOCK_WIDTH = 54;
+	static int BLOCK_HEIGHT = 7;
 	private PackedScene _blockScene = (PackedScene)GD.Load("res://block.tscn");
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Player player = (Player)GetNode("Player");
+		Vector2 PlayerPos = Vector2.Zero;
+		PlayerPos.Y = (int)GetViewportRect().Size.Y -100;
+		player.Position = PlayerPos;
+		
 		MakeFloor();
 
 		ScatterBlocks();
@@ -26,15 +32,30 @@ public partial class level1 : Node2D
 			block.Position = new Vector2(x: RanX, y: RanY);
 			AddChild(block);
 		}
+
+
+		for (int i = 0; i < 30; i++)
+		{
+			Node2D block = (Node2D)_blockScene.Instantiate();
+			block.Position = new Vector2(x: 0, y: i * (BLOCK_HEIGHT + 30));
+			AddChild(block);
+		}
 	}
 
 	private void MakeFloor()
 	{
-		for (int i = 0; i < 25; i++)
+		int FloorLength = 0;
+		int i = 0;
+
+		while (FloorLength < (int)GetViewportRect().Size.X)
 		{
+			FloorLength = i * BLOCK_WIDTH;
+
 			Node2D block = (Node2D)_blockScene.Instantiate();
-			block.Position = new Vector2(x: i * BLOCK_WIDTH, y: (int)GetViewportRect().Size.Y);
+			block.Position = new Vector2(x: FloorLength, y: (int)GetViewportRect().Size.Y);
 			AddChild(block);
+			
+			i++;
 		}
 	}
 
